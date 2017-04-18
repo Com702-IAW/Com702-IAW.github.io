ff31c884c9461dec0b8b8e6b991ba1fb490a4acf
Manuel Rios <manuel.ir2302@gmail.com>
2017-04-18 19:18:03 -0300

    Commit Lógica terminada
    Se logro hacer la lógica de la aplicación (Modelo, Vista y Controlador).
    Se añadió la lista del carrito donde se guarda los componentes que el usuario desea.
    Se calcula el precio del carrito seleccionado.
    Se completaron los menúes desplegables.
    
    Queda:
    - Completar los 2 CSS
    - Agregar mas elementos al Json
    - Nombre y Logo de la Empresa
    - Recordar los cambios del usuario

---
+++ js/controlador.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var jsonComponentes,jsonCarrito;
+
+$(function () {
+    var arreglo = ordenarComponentes();
+    mostrar(arreglo);
+});
+
+function ordenarComponentes() {
+    var index, index1, cont;
+    cont = 0;
+    var arreglo = new Array();
+
+    for (index = 0; index < jsonComponentes.length; ++index) {
+        var objet = jsonComponentes[index];
+        for (index1 = 0; index1 < objet.length; ++index1) {
+            var objeto = objet[index1];
+            objeto.id = index;      //para ubicar en primer arreglo en json
+            objeto.id2 = index1;    //para ubicar en segund arreglo en json (dentro de tipo de componente)
+            objeto.id3 = cont;      //para ubicar en el arreglo nuevo creado con todos los componentes
+    //        objeto.pedido = false;
+            arreglo[cont] = objeto;
+            ++cont;            
+        }
+    }
+     return arreglo;    
+}
+
+function actualizarPedido(componente) {
+    jsonCarrito[componente.id] = componente;
+    $("#imagen"+componente.id).attr("src",componente.imagen);
+    computarTotal();
+}
+
+function computarTotal(){
+    var total,index;
+    total = 0;
+    
+    for (index = 0; index < jsonCarrito.length; ++index){
+        var lala2 = jsonCarrito[index];
+        if (lala2 !== null)
+            total = total + lala2.precio;
+    }
+    
+    $("#preciototal").text("El precio total es: $"+total);
+}
+
+
---
+++ js/modeloComponentes.js
@@ -0,0 +1,70 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var jsonComponentes = [
+		monitor = [
+		{	"marca": "Samsung",
+			"precio": 1000,
+                        "color" : "Negro",
+                        "imagen": "src/monitor0.png"
+		},
+		{
+			"marca": "Benq",
+			"precio": 2000,
+                        "color" : "Blanco",
+                        "imagen" : "src/monitor1.png"
+		}
+		]
+		,
+		teclado = [
+		{	"marca": "Genius",
+			"precio": 500,
+                        "color" : "Negro",
+                        "imagen" : "src/teclado0.png"
+		},
+		{
+			"marca": "Lark",
+			"precio": 400,
+                        "color" : "Blanco",
+                        "imagen" : "src/teclado1.png"
+		}
+		],
+                mouse = [
+		{	"marca": "Genius",
+			"precio": 200,
+                        "color" : "Negro",
+                        "imagen" : "src/mouse0.png"
+		},
+		{
+			"marca": "Microsoft",
+			"precio": 300,
+                        "color" : "Gris",
+                        "imagen" : "src/mouse1.png"
+		}
+                ],
+                parlantes = [
+		{	"marca": "Genius",
+			"precio": 400,
+                        "color" : "Negro",
+                        "imagen" : "src/parlantes0.png"
+		},
+		{
+			"marca": "Genius",
+			"precio": 600,
+                        "color" : "Blanco y Verde",
+                        "imagen" : "src/parlantes1.png"
+		}
+                ]
+];
+
+var jsonCarrito = [
+    monitor = null,
+    teclado = null,
+    mouse = null,
+    parlantes = null
+];
+
+
+
